b1818a672a0023882b65f6583e413d0c325a3ac8
saaramahmoudi <sin29mim@gmail.com>
2021-01-20 01:38:50 -0500

    Api removed temp

--- tweet/pkg/handler/lib.go
+++ tweet/pkg/handler/lib.go
@@ -1,19 +1,19 @@
 package handler
-
-import (
-	"github.com/saaramahmoudi/twitter-backend/tweet/internal/core/ports"
-	"github.com/saaramahmoudi/twitter-backend/tweet/internal/core/service"
-	"github.com/saaramahmoudi/twitter-backend/tweet/internal/repositories"
-)
-
-type tweetApi struct {
-	ports.TweetService
-}
-
-var TweetApi = tweetApi{service.TweetService{Repo :repositories.TweetFirestore{}}}
-
-
-
+//
+//import (
+//	"github.com/saaramahmoudi/twitter-backend/tweet/internal/core/ports"
+//	"github.com/saaramahmoudi/twitter-backend/tweet/internal/core/service"
+//	"github.com/saaramahmoudi/twitter-backend/tweet/internal/repositories"
+//)
+//
+//type tweetApi struct {
+//	ports.TweetService
+//}
+//
+//var TweetApi = tweetApi{service.TweetService{Repo :repositories.TweetFirestore{}}}
+//
+//
+//
 
 
 
